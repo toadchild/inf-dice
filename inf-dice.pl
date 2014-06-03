6d3b8daf181e8dc67377de1d0d9b6543c28f0994
jonathan <jonathan@adaf7115-cb76-40e5-8e63-e26f26d75cb9>
2014-06-03 17:40:31 +0000

    Slightly cleaner way of checking if there is an Operator.

--- inf-dice.pl
+++ inf-dice.pl
@@ -663,7 +663,7 @@ sub print_player_output{
             if(!$disabled_h){
                 $disabled_h = $h;
             }
-        }elsif($eject >= 0 && $w > $eject){
+        }elsif($operator_w && $w > $eject){
             $label = ' (Operator ' . ($wounds + $operator_w - $w) . ' W)';
         }else{
             $label = ' (' . ($wounds - $w) . " $w_type)";
