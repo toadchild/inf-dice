547510a97a408543c8be13a746939709c4f7f0a2
Jonathan Polley <jonathan@ghostlords.com>
2018-05-29 21:26:46 -0700

    Update upstream data URL

--- add_infinitydata_git.sh
+++ add_infinitydata_git.sh
@@ -12,5 +12,5 @@ rm -rf unit_data
 mkdir unit_data
 cd unit_data
 git init
-git remote add -f origin https://${USER}@bitbucket.org/cgordon13/infinitydata.git
+git remote add -f origin https://${USER}@bitbucket.org/toadchild/infinitydata.git
 git checkout $BRANCH
