4217fc551838b06f1abb3ab60e558c3fa15cdb91
Jonathan Polley <jonathan@ghostlords.com>
2020-09-08 21:29:05 -0700

    Add additional test for multi-save ammo types

--- test.pl
+++ test.pl
@@ -78,6 +78,67 @@ $output = `./inf-dice-n4 9 3 1 4 NONE 12 1 1 12 NONE`;
 die if $output ne $expected;
 
 ###########################################################################
+# Test FtF of DA vs EXP
+###########################################################################
+
+$expected = <<EOF;
+P1 STAT 12 CRIT 12 CRIT_1 N BOOST  0 B 1 TEMPLATE 0 AMMO NORMAL DAM[0]  2 TAG[0] NONE DAM[1]  2 TAG[1] NONE
+P2 STAT 12 CRIT 12 CRIT_1 N BOOST  0 B 1 TEMPLATE 0 AMMO NORMAL DAM[0] 10 TAG[0] NONE DAM[1] 10 TAG[1] NONE DAM[2] 10 TAG[2] NONE
+
+Total Rolls: 400
+Actual Rolls Made: 169
+Savings: 57.75%
+
+P1 Hits:  0 Crits:  1 -  4.750% (19)
+P1 Hits:  1 Crits:  0 - 35.750% (143)
+
+No Hits: 19.000% 76
+
+P2 Hits:  0 Crits:  1 -  4.750% (19)
+P2 Hits:  1 Crits:  0 - 35.750% (143)
+
+
+======================================================
+
+P1 Scores  3 Success(es):  0.005% NONE
+P1 Scores  3 Success(es):  0.005%
+P1 Scores  2 Success(es):  0.486% NONE
+P1 Scores  2 Success(es):  0.486%
+P1 Scores  1 Success(es):  7.589% NONE
+P1 Scores  1 Success(es):  7.589%
+P1 Scores  3+ Successes:   0.005% NONE
+P1 Scores  2+ Successes:   0.491% NONE
+P1 Scores  1+ Successes:   8.080% NONE
+P1 Scores  3+ Successes:   0.005%
+P1 Scores  2+ Successes:   0.491%
+P1 Scores  1+ Successes:   8.080%
+
+No Successes: 56.186%
+
+P2 Scores  4 Success(es):  0.297% NONE
+P2 Scores  4 Success(es):  0.297%
+P2 Scores  3 Success(es):  5.656% NONE
+P2 Scores  3 Success(es):  5.656%
+P2 Scores  2 Success(es): 15.188% NONE
+P2 Scores  2 Success(es): 15.188%
+P2 Scores  1 Success(es): 14.594% NONE
+P2 Scores  1 Success(es): 14.594%
+P2 Scores  4+ Successes:   0.297% NONE
+P2 Scores  3+ Successes:   5.953% NONE
+P2 Scores  2+ Successes:  21.141% NONE
+P2 Scores  1+ Successes:  35.734% NONE
+P2 Scores  4+ Successes:   0.297%
+P2 Scores  3+ Successes:   5.953%
+P2 Scores  2+ Successes:  21.141%
+P2 Scores  1+ Successes:  35.734%
+
+EOF
+
+$output = `./inf-dice-n4 12 1 2 2 NONE 2 NONE 12 1 3 10 NONE 10 NONE 10 NONE`;
+
+die if $output ne $expected;
+
+###########################################################################
 # Done!
 ###########################################################################
 
